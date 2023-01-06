b817c2902ca51a9cbdabe768b4695bb3985399e4
SullivanCaballero <bcpajuelo@gmail.com>
2023-01-06 21:38:46 +0100

    enseñando a beatris como va esto
    Soy Alehandro y doy clazez de unitý

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -29,7 +29,7 @@ public class PlayerControl : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         Animator = gameObject.GetComponent<Animator>();
-        Debug.Log("Holaa");
+        Debug.Log("Test");
         
     }
 
