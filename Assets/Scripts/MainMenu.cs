2f4d9a1a0b2ecc1745a93d3bbd7bb3f6eaf3c790
Thisistriis <bcpajuelo@gmail.com>
2023-01-07 23:43:28 +0100

    Menu Ready

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  
+    public void PlayGame ()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame ()
     {
-        
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void Settings ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
