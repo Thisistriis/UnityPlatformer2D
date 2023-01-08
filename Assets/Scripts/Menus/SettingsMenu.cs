b6b134f2ed666d8624944088af8cc17e8ab5eaed
Thisistriis <bcpajuelo@gmail.com>
2023-01-08 19:32:15 +0100

    buttons

---
+++ Assets/Scripts/Menus/SettingsMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public void Return()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
