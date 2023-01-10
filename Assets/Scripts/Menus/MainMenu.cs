4eae15537bead75aaffbdaf9fda01d40ddf4e3c6
Thisistriis <bcpajuelo@gmail.com>
2023-01-10 16:16:04 +0100

    Wait

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
   
     public void PlayGame ()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(3);
     }
 
     public void QuitGame ()
